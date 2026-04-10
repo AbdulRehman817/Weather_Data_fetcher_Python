a1fa9dad7b6a0179f46526bca5890c5bd21911b5
AbdulRehman817 <abdulrehmanbey1718@gmail.com>
2026-04-10 22:36:05 +0500

    updated main.py

--- main.py
+++ main.py
@@ -6,29 +6,38 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
     weather = None
+    error = None
 
     if request.method == "POST":
         try:
-            city = request.form["city"]
+            city = request.form.get("city")
 
+            # Get city location
             geo = requests.get(
-                f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1"
+                f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1",
+                timeout=5
             ).json()
 
-            if "results" in geo:
+            # Check city exists
+            if geo.get("results"):
                 lat = geo["results"][0]["latitude"]
                 lon = geo["results"][0]["longitude"]
 
+                # Get weather
                 data = requests.get(
-                    f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,relative_humidity_2m"
+                    f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,relative_humidity_2m",
+                    timeout=5
                 ).json()
 
-                weather = data["current"]
+                weather = data.get("current")
+            else:
+                error = "City not found"
 
-        except:
-            weather = None
+        except Exception as e:
+            error = "Something went wrong"
 
-    return render_template("index.html", weather=weather)
+    return render_template("index.html", weather=weather, error=error)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
